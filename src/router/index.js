65c00586dd8325591ee1869526d4e81fce3511fe
dsdvvv <2473378640@qq.com>
2019-12-21 19:30:19 +0800

    新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '@/views/home/Home' // 首页
+import Login from '@/views/login/login' // 登录页
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/home' // 重定向
+  },
+  // 首页
+  {
+    path: '/home',
     name: 'home',
     component: Home
+  },
+  // 登录页
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
   // {
   //   path: '/about',
